2c14524d024f3299be2097c09d17ecf8121c2c73
moparisthebest <admin@moparisthebest.com>
2010-04-24 17:48:12 -0400

    Finished SecurityManager permissions, and it is now ready for custom clients and servers.

--- src/org/moparscape/security/SecurityManager.java
+++ src/org/moparscape/security/SecurityManager.java
@@ -16,17 +16,28 @@ public class SecurityManager extends java.lang.SecurityManager {
 
     private Map<ClassLoader, Permissions> permissionMap = new HashMap<ClassLoader, Permissions>();
 
-    // two permissions allowed if using java.util.GregorianCalendar
+    // single socket permission that is allowed
+    private java.net.SocketPermission allowedSocket = new java.net.SocketPermission("localhost", "connect,accept,resolve");
+
+    // some permissions we need for special cases
     private Permission p1 = new java.lang.RuntimePermission("accessClassInPackage.sun.util.resources");
     private Permission reflectPerm = new java.lang.reflect.ReflectPermission("suppressAccessChecks");
     private Permission classLoaderPerm = new java.lang.RuntimePermission("createClassLoader");
 
     public void addPermissions(ClassLoader cl, Permissions perms) {
+        // if they can't set the SecurityManager, they shouldn't be able to modify this one, so check...
+        System.getSecurityManager().checkPermission(new java.lang.RuntimePermission("setSecurityManager"));
         //if the key already exists, just return, we only support setting the permissions once
         if (permissionMap.containsKey(cl))
             return;
         perms.setReadOnly();
         permissionMap.put(cl, perms);
+    }
+
+    public void allowSocketTo(String host){
+        // if they can't set the SecurityManager, they shouldn't be able to modify this one, so check...
+        System.getSecurityManager().checkPermission(new java.lang.RuntimePermission("setSecurityManager"));
+        allowedSocket = new java.net.SocketPermission(host, "connect,accept,resolve");
     }
 
     /**
@@ -51,6 +62,10 @@ public class SecurityManager extends java.lang.SecurityManager {
 
         // this isn't ready to go live yet, so just return and allow it all
         //if(true) return;
+
+        // if this is the allowed socket, allow it
+        if(allowedSocket.implies(perm))
+                return;
 
         // we are now going to go through the ClassLoaders of all Classes on the stack
         // if any of them are in perms, then check the permissions, sticking to the most
@@ -77,7 +92,11 @@ public class SecurityManager extends java.lang.SecurityManager {
             if (lastCName.startsWith("java.util.") && lastCName.endsWith("Calendar") && (perm.equals(p1) || perm.equals(reflectPerm)))
                 return;
             // some more exceptions for when java classes use reflection. why?...
-            if (lastCName.equals("javax.sound.sampled.AudioSystem") && perm.equals(reflectPerm))
+            // also an exception for jsound, we can't just allow loadlibrary.jsound* because there could be malicious
+            // code in a custom library named jsoundhacks and it would be allowed to load, so we only allow
+            // loadlibrary.jsound* if the next class on the stack is javax.sound.sampled.AudioSystem
+            // known examples are jsoundds and jsoundalsa
+            if (lastCName.equals("javax.sound.sampled.AudioSystem") && (perm.equals(reflectPerm) || perm.getName().startsWith("loadLibrary.jsound")))
                 return;
             if (lastCName.equals("java.awt.Component") || lastCName.equals("sun.font.FontDesignMetrics")) {
                 // component uses reflection, and sometimes creates a classloader, yay...
@@ -106,11 +125,13 @@ public class SecurityManager extends java.lang.SecurityManager {
         if (allow)
             return;
 
-        System.out.println("denying: " + perm.toString());
+        if(org.moparscape.MainPanel.debug()){
+            System.out.println("denying: " + perm.toString());
 
-        // class stack for debugging
-        for (int i = 1; i < c.length; i++) System.out.println(i + ": " + c[i].getName());
-        Thread.dumpStack();
+            // class stack for debugging
+            for (int i = 1; i < c.length; i++) System.out.println(i + ": " + c[i].getName());
+                Thread.dumpStack();
+        }
 
         // otherwise allow is false, throw a SecurityException
         throw new SecurityException("Permission denied: "+perm.toString());
